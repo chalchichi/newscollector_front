06c611f2831aaee80c05c39d9c97b6d3a56f3fb8
ohyunhu <dhdbsgn111@naver.com>
2021-01-14 23:51:46 +0900

    기본 테이블 템플릿 적용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './Costomer'
 import './App.css';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customers = [
+{
+'id': 1,
+'image': 'https://placeimg.com/64/64/1',
+'name': '홍길동',
+'birthday': '961222',
+'gender': '남자',
+'job': '대학생'
+},
+{
+'id': 2,
+'image': 'https://placeimg.com/64/64/2',
+'name': '나동빈',
+'birthday': '960508',
+'gender': '남자',
+'job': '프로그래머'
+},
+{
+'id': 3,
+'image': 'https://placeimg.com/64/64/3',
+'name': '이순신',
+'birthday': '961127',
+'gender': '남자',
+'job': '디자이너'
+}
+]
+
+class App extends Component {
+render() {
+return (
+<div>
+<Table>
+<TableHead>
+<TableRow>
+<TableCell>번호</TableCell>
+<TableCell>이미지</TableCell>
+<TableCell>이름</TableCell>
+<TableCell>생년월일</TableCell>
+<TableCell>성별</TableCell>
+<TableCell>직업</TableCell>
+</TableRow>
+</TableHead>
+<TableBody>
+{customers.map(c => {
+return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+})}
+</TableBody>
+</Table>
+</div>
+);
+}
 }
 
 export default App;
+
